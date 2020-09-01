77cabf67356fa9ec47b1bf730a035feaa1a159b4
高俊伟 <gjw_2007@163.com>
2020-09-01 18:13:17 +0800

    Update BarcodeScanPlugin.h

--- ios/Classes/BarcodeScanPlugin.h
+++ ios/Classes/BarcodeScanPlugin.h
@@ -1,5 +1,4 @@
 #import <Flutter/Flutter.h>
-#import "BarcodeScannerViewControllerDelegate.h"
 
 #define UIColorFromRGB(rgbValue) \
 [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
@@ -7,7 +6,7 @@
   blue:((float)((rgbValue & 0x0000FF) >>  0))/255.0 \
   alpha:1.0]
 
-@interface BarcodeScanPlugin : NSObject<FlutterPlugin, BarcodeScannerViewControllerDelegate>
+@interface BarcodeScanPlugin : NSObject<FlutterPlugin>
 
 @property(nonatomic, copy) FlutterResult result;
 @property (nonatomic, assign) UIViewController *hostViewController;
